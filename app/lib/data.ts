24a2ddbf9aa5f894f21e21d3f53d154e99f32022
whit33y <troppout@gmail.com>
2023-12-06 12:00:07 +0100

    All reports fetch to db created

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,12 +1,33 @@
-import { CategoriesTable } from "./types";
+import { CategoriesTable, ReportsTable } from "./types";
 import { sql } from "@vercel/postgres";
+
+export async function fetchAllReports() {
+  try {
+    const data = await sql<ReportsTable>`
+        SELECT
+        id,
+        topic,
+        hd_number,
+        create_date,
+        category,
+        text,
+        user_id,
+        status,
+        images FROM reports
+        `;
+    const reports = data.rows;
+    return reports;
+  } catch (err) {
+    console.error("Database error: ", err);
+  }
+}
+
 export async function fetchCategories() {
   try {
     const data = await sql<CategoriesTable>`
         SELECT
         name
         FROM categories
-        ORDER BY name
         `;
     const categories = data.rows;
     return categories;
